a2accb84d7077a55f16be03f253ca6cd0a2d78f3
Prabhdeep035 <prabhdeep9953@gmail.com>
2025-05-26 14:42:06 +0530

    added responsiveness

--- app.js
+++ app.js
@@ -2,42 +2,64 @@ const inp=document.getElementById("input");
 const add=document.querySelector("#add");
 const work=document.getElementById("work");
 const del=document.getElementsByClassName("del");
+const done=document.getElementsByClassName("done");
 
 const wo=[];
-const arr=[];
 let i=1;
 
 function addWork(){
     wo.push(inp.value);
-    i=wo.length;
     inp.value="";
-    arr.push(`
-          <div id="to-do"> 
-            <input type="checkbox" name="done" class="done" id="check-${i}">
-            <p id="p-${i}">${wo[i-1]}</p>
-            <button class="del" id="${i}">del</button>
-          </div>`
-    )
     show();
 }
 
 function deleteWork(i){
-    arr.splice(i,1);
     wo.splice(i,1);
-    console.log(wo);
+    console.log(i);
     show();
+}
+
+function workDone(i){
+    const temp=document.getElementById(`p-${i}`);
+    temp.style.textDecoration="line-through";
+    temp.style.opacity="0.5"
+    
+}
+
+function workUndo(i){
+    const temp=document.getElementById(`p-${i}`);
+    temp.style.textDecoration="none"
+    temp.style.opacity="1"
 }
 
 function show(){
     work.innerHTML="";
-    for(i=arr.length;i>=1;i--){
-        work.innerHTML+=arr[i-1];
+    for(i=wo.length;i>=1;i--){
+        work.innerHTML+=`
+          <div id="to-do"> 
+            <input type="checkbox" name="done" class="done" id="${i}">
+            <div class="para-container">
+               <p class="para" id="p-${i}">${wo[i-1]}</p>
+            </div>
+            <button class="del" id="${i}">X</button>
+          </div>`;
     }
 
     for (let btn of del){
         btn.addEventListener("click",function(){
-        deleteWork(this.id-1);
+            deleteWork(this.id-1);
         });
+    }
+
+    for (let check of done){
+        check.addEventListener("click",function(){
+            if(check.checked){
+                workDone(this.id);
+            }
+            else{
+                workUndo(this.id);
+            }
+        })
     }
 }
 
@@ -47,4 +69,3 @@ inp.addEventListener("keydown",function(e){
         addWork();
     }
 })
-
